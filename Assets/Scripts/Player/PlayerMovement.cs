ea8ea3fc02904eeb3f256e4cc2859203a73773b8
JamiroDV <vulderj@talnet.nl>
2025-09-29 10:16:46 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Ik wil heel graag dat hier voor geweldige playermovement komt");
+        print("PlayerMovement Done");
+        print("Ik wil heel graag dat voor deze print geweldige playermovement komt");
     }
 
     // Update is called once per frame
