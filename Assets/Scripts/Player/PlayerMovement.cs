dec3b666c4b61dc40bc03f5f15079f65d65d28d7
JamiroDV <vulderj@talnet.nl>
2025-09-29 10:18:34 +0200

    Updated camera and lvl

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        print("Camera Werk is done");
         print("Ik wil heel graag dat hier voor geweldige playermovement komt");
     }
 
